089576c87588e80e224eb6b773789007582fc97d
Iftekhar Khaled <iftekharkhaled1@outlook.com>
2022-05-12 21:51:04 +0600

    Packaged all the data

---
+++ stations.py
@@ -0,0 +1,41 @@
+"""
+This file only contains the names and URLS of the Divisions and their respective weather stations.
+The variables are defined as a nested list where the first element is a list of URLS and the second element is the name of the Division.
+Example usage: [["url1", "url2"], "Division"]
+
+In case of change in the weather station URLS pkease edit the urls in the first item in the nested list.
+The order does not matter.
+"""
+
+dhk_urlz = [['http://live4.bmd.gov.bd/map/1/#Dhaka', 'http://live4.bmd.gov.bd/map/1/#Tangail',
+'http://live4.bmd.gov.bd/map/1/#Faridpur', 'http://live4.bmd.gov.bd/map/1/#Madaripur', 'http://live4.bmd.gov.bd/map/1/#Gopalganj',
+'http://live4.bmd.gov.bd/map/1/#Nikli'], "Dhaka"]
+
+
+ctg_urlz = [['http://live4.bmd.gov.bd/map/4/', 'http://live4.bmd.gov.bd/map/4/#Saint%20Martin',
+'http://live4.bmd.gov.bd/map/4/#Sandwip', 
+'http://live4.bmd.gov.bd/map/4/#Sitakundu' , 'http://live4.bmd.gov.bd/map/4/#Cumilla' , 
+'http://live4.bmd.gov.bd/map/4/#Feni' , 'http://live4.bmd.gov.bd/map/4/#Hatiya', 'http://live4.bmd.gov.bd/map/4/#Kutubdia' , 
+'http://live4.bmd.gov.bd/map/4/#Teknaf', 'http://live4.bmd.gov.bd/map/4/#Dighinala', 'http://live4.bmd.gov.bd/map/4/#Bandarban', 
+'http://live4.bmd.gov.bd/map/4/#Maijdi%20Court', "http://live4.bmd.gov.bd/map/4/#Cox's%20Bazar"], "Chittagong"]
+
+
+syl_url = [['http://live4.bmd.gov.bd/map/7/', 'http://live4.bmd.gov.bd/map/7/#Srimangal'], "Sylhet" ]
+
+
+mymensingh_url = [['http://live4.bmd.gov.bd/map/18/', 'http://live4.bmd.gov.bd/map/18/#Netrokona'], "Mymensingh"]
+
+
+raj_url = [['http://live4.bmd.gov.bd/map/77/', 'http://live4.bmd.gov.bd/map/77/#Ishurdi', 'http://live4.bmd.gov.bd/map/77/#Bogura', 'http://live4.bmd.gov.bd/map/77/#Badalgachhi',
+'http://live4.bmd.gov.bd/map/77/#Tarash'], "Rajshahi"]
+
+
+rang_url = [['http://live4.bmd.gov.bd/map/82/', 'http://live4.bmd.gov.bd/map/82/#Dinajpur', 'http://live4.bmd.gov.bd/map/82/#Sayedpur' , 'http://live4.bmd.gov.bd/map/82/#Rajarhat',
+'http://live4.bmd.gov.bd/map/82/#Dimla', 'http://live4.bmd.gov.bd/map/82/#Tetulia'], "Rangpur"]
+
+
+khul_url = [['http://live4.bmd.gov.bd/map/88/' , 'http://live4.bmd.gov.bd/map/88/#Mongla', 'http://live4.bmd.gov.bd/map/88/#Satkhira',
+'http://live4.bmd.gov.bd/map/88/#Jashore', 'http://live4.bmd.gov.bd/map/88/#Chuadanga', 'http://live4.bmd.gov.bd/map/88/#Kumarkhali'], "Khulna"]
+
+
+bar_url = [['http://live4.bmd.gov.bd/map/94/', 'http://live4.bmd.gov.bd/map/94/#Patuakhali', 'http://live4.bmd.gov.bd/map/94/#Khepupara', 'http://live4.bmd.gov.bd/map/94/#Bhola'], "Barisal"]
